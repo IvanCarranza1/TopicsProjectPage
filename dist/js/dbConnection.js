e1532d12ca7b0f87ca681f9ad885c9081f10082d
IvanCarranza1 <ivancarranzadev@gmail.com>
2022-10-25 00:20:56 -0500

    Conexion a BD

---
+++ dist/js/dbConnection.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAK9kdgri_55Qnh5XE2CU6s3h4ARcEnXvE",
+  authDomain: "ruta-gris.firebaseapp.com",
+  projectId: "ruta-gris",
+  storageBucket: "ruta-gris.appspot.com",
+  messagingSenderId: "74283898832",
+  appId: "1:74283898832:web:a5a919fc4647697d234bd5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
